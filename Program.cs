5f102a93a8af9255c26186e770d21b8277c25326
Erkebulan <kaliev.e.m@gmail.com>
2023-01-23 23:27:31 +0600

    PolindromeLesson4SeminarWork1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+number = int.Parse(Console.ReadLine()!);
+int exampleOfFiveDigitNumber = 34593;
+string strTwo = $"{exampleOfFiveDigitNumber}";
+string strOne = $"{number}";
+int lengthOne = strOne.Length;
+int lengthTwo = strTwo.Length;
+int lengthDifference =lengthOne-lengthTwo;
+int degree = Convert.ToInt32(Math.Pow(10,lengthDifference));
+if (lengthTwo==lengthOne)
+{
+    int a = number/1000;
+    int b = number%100;
+    int endOfNumber = a%10;
+    int penultimateNumber = a/10;
+    int multiple = endOfNumber*10;
+    int plus = multiple+penultimateNumber;
+    if (plus==b)
+    {
+    Console.WriteLine("да");
+    }
+    else
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
